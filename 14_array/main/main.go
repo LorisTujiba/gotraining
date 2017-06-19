5f7418eb5ac1113f34be9d5989f15b767f4efbb0
Loris Tujiba Soejonopoetro <pisceslrz@gmail.com>
2017-06-19 14:33:57 +0700

    Added array and slice

---
+++ 14_array/main/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main(){
+
+/*==============================
+Array
+============================
+Sequence of elements of a single
+type. not dynamic, unlike
+slice. Slice is dynamic.
+*/
+
+
+	var numbers [10]int //array
+	var scores []int //slice
+
+	numbers[4] = 4
+
+	for i,v := range numbers{
+		fmt.Println(i," ",v)
+
+	}
+
+	scores = append(scores, 10,11,12,31,42)
+
+	for i,v := range scores{
+		fmt.Println(i," ",v)
+	}
+}
---
+++ 15_slice/main/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+/*==============================
+Slice
+============================
+A referenced type. Slice is
+dynamic. It's indexable
+and have a length.
+*/
+
+func main(){
+	theSlice := []int{12,13,14,15,16,17,18,19}
+	fmt.Println(theSlice)//print all
+	fmt.Println(theSlice[2:5])//print from index 2 to 4, 5 not included
+	fmt.Println(theSlice[2])//print at index 2
+	fmt.Println("get this A's ASCII"[9])//get string's char at index of 9. which is A. and ASCII of A is 65
+
+	anotherSlice := make([]int,0,3)//create a slice that has 3 capacity and 0 len
+
+	for i:=0;i<20;i++{
+		anotherSlice = append(anotherSlice, i)
+		fmt.Println("capacity : ",cap(anotherSlice),"Len : ",len(anotherSlice))
+	}//if the len exceeds the capacity, go will twice the capacity
+
+	//fmt.Println(anotherSlice[51])//index out of range, only use append if you want to extend the capacity
+	theSlice = append(theSlice,anotherSlice...)//appending 2 slice
+	fmt.Println(theSlice)
+}
